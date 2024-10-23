27a73321001b39407f0363ac46ef22adb395e924
Xica Felix <xicasfelix@gmail.com>
2024-10-23 17:52:17 -0400

    finish implementing stretch goal 1, begin implementing stretch goal 2

--- bank_account.py
+++ bank_account.py
@@ -2,6 +2,7 @@ import random
 random_account = str(random.randrange(10000000, 99999999,1))
 
 class BankAccount:
+    # Implement account_type stretch goal
     def __init__(self, full_name:str , account_type: str, account_number = random_account, balance: int = 0) -> None:
         self.full_name = full_name
         self.account_number = account_number
@@ -25,6 +26,7 @@ class BankAccount:
         return self.balance
     
     def add_interest(self):
+        # Implement stretch goal
         if(self.account_type == 'savings'):
             self.balance*=1.1
         else:
@@ -41,3 +43,20 @@ account_one.print_statement()
 account_one.add_interest()
 account_one.withdraw(150)
 account_one.print_statement()
+
+# Stretch Goal #1
+account_two = BankAccount('Person A', 'checking')
+account_two.deposit(1500)
+
+account_three = BankAccount('Person A', 'savings')
+account_three.deposit(464783)
+
+# Stretch Goal #2
+bank = list()
+bank.append(account_two)
+bank.append(account_three)
+
+for account in bank:
+    account.add_interest()
+    
+
