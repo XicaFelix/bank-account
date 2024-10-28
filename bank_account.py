e17e3a007f5fa272734a3d6337bcef6e1eb7ba61
Xica Felix <xicasfelix@gmail.com>
2024-10-28 15:21:02 -0400

    revise random account generator to avoid using same account number when called

--- bank_account.py
+++ bank_account.py
@@ -1,13 +1,17 @@
-import random
-random_account = str(random.randrange(10000000, 99999999,1))
+import random,time
+
 
 class BankAccount:
     # Implement account_type stretch goal
-    def __init__(self, full_name:str , account_type: str, account_number = random_account, balance: int = 0) -> None:
+    def __init__(self, full_name:str , account_type: str, account_number = None, balance: int = 0) -> None:
         self.full_name = full_name
-        self.account_number = account_number
+        self.account_number =  account_number if account_number else self.random_account()
         self.balance = balance
         self.account_type = account_type
+
+    def random_account(self):
+        random.seed(time.time())
+        return str(random.randrange(10000000, 99999999,1))
 
     def deposit(self, amount):
         self.balance += amount
@@ -34,7 +38,7 @@ class BankAccount:
 
     def print_statement(self):
         output = f"{self.full_name}\nAccount No.: {self.account_number}\nBalance: ${self.balance}"
-        print(output)
+        print('\n',output)
 
 
 account_one = BankAccount('Mitchell', 'checking','03141582')
@@ -44,12 +48,21 @@ account_one.add_interest()
 account_one.withdraw(150)
 account_one.print_statement()
 
-# Stretch Goal #1
-account_two = BankAccount('Person A', 'checking')
-account_two.deposit(1500)
+account_two = BankAccount('Bloopo', 'checking')
+account_two.deposit(100)
+account_two.print_statement()
 
-account_three = BankAccount('Person A', 'savings')
-account_three.deposit(464783)
+account_three = BankAccount('Ann', 'savings',balance=200)
+account_three.add_interest()
+account_three.withdraw(170)
+account_three.print_statement()
+
+# Stretch Goal #1
+account_four = BankAccount('Person A', 'checking')
+account_four.deposit(1500)
+
+account_five = BankAccount('Person A', 'savings')
+account_five.deposit(464783)
 
 # Stretch Goal #2
 bank = list()
